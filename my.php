328b4999106e2abae714646f920848dcbf31bacf
V02ch12k0 <Nastya21052105@mail.ru>
2024-03-25 14:27:01 +0500

    modified:   index.html 	new file:   index.php 	modified:   my.php 	new file:   my2.php

--- my.php
+++ my.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['login']) && isset($_POST['password'])) {
-        $login = $_POST['login'];
         $password = $_POST['password'];
+        $login = $_POST['login'];
         
         if ($login !== '' && $password !== '') {
             echo "<h1>Welcome, $login!</h1>";
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>мяу мяу мяу</title>
 </head>
 <style>
     h1 {
